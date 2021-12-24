53204bec332d4bbd8a8d663e9497427a927dee4f
ingong <insong1216@gmail.com>
2021-12-25 01:29:20 +0900

    feat: store 함수 정의

---
+++ src/store/createStore.js
@@ -0,0 +1,25 @@
+export const createStore = (reducer) => {
+  let state = [];
+  const listener = [];
+
+  const publish = () => {
+    listener.forEach(({ subscriber, context }) => {
+      subscriber.call(context);
+    });
+  };
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    publish();
+  };
+
+  const subscribe = (subscriber, context = null) => listener.push({ subscriber, context });
+
+  const getState = () => [...state];
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+};
