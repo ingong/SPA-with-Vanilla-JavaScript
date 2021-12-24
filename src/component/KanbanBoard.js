9c9766379e33e7ec60932ceb0e7b20a5333ef3ca
ingong <insong1216@gmail.com>
2021-12-24 23:51:14 +0900

    feat: 칸반 보드에 3가지 상태를 갖는 배열 주입

--- src/component/KanbanBoard.js
+++ src/component/KanbanBoard.js
@@ -1,7 +1,17 @@
 import Component from '@/common/component';
-
+import KanbanColumn from '@/component/KanbanColumn';
+import '@/style/kanban.css';
 export default class KanbanBoard extends Component {
   template() {
-    return `<div>Kanban Board</div>`;
+    return `
+    <main>
+      <h1>칸반 보드</h1>
+      <section class="kanban-container">
+      </section>
+    </main>
+    `;
+  }
+  renderChildren() {
+    this.$state.map((v) => new KanbanColumn('.kanban-container', { name: v, list: [] }));
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import KanbanBoard from '@/component/KanbanBoard';
 import '@/style/index.css';
 
 const App = {
-  init: () => new KanbanBoard('#root'),
+  init: () => new KanbanBoard('#root', ['TODO', 'IN_PROGRESS', 'DONE']),
 };
 
 document.addEventListener('DOMContentLoaded', App.init());
