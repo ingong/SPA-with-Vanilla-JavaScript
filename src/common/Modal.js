dc8a4555f94264d7aa130b14257847c8d951b4c1
ingong <insong1216@gmail.com>
2021-12-25 13:25:50 +0900

    feat: 공통으로 사용될 칸반 모달 컴포넌트 작성
    - 삭제하는 경우와 수정 / 생성의 경우 공통의 모달 컴포넌트에서 사용하기 위함

---
+++ src/common/Modal.js
@@ -0,0 +1,33 @@
+import Component from '@/common/Component';
+import { qs } from '@/utils/helper';
+import '@/style/modal.css';
+import { store, deleteItem } from '@/store/index';
+
+export default class Modal extends Component {
+  template() {
+    return `
+      <div class="modal hidden">
+        <div class="modal__overlay"/>
+        <div class="modal__content">
+          <h2>정말로 아이템을 삭제하시겠습니까?</h2>
+          <div class="modal__button-container">
+            <button class="modal__cancel">취소</button>
+            <button class="modal__confirm">확인</button>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  setEvent() {
+    const closeModal = () => qs('.modal').classList.add('hidden');
+    const confirmModalBtn = () => {
+      closeModal();
+      store.dispatch(deleteItem('ISSUE-104'));
+    };
+
+    qs('.modal__overlay').addEventListener('click', closeModal);
+    qs('.modal__cancel').addEventListener('click', closeModal);
+    qs('.modal__confirm').addEventListener('click', confirmModalBtn);
+  }
+}
