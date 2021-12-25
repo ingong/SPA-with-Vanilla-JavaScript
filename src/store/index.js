30e410c4834c2fba722350dbf7cb6cfe57852935
ingong <insong1216@gmail.com>
2021-12-25 13:19:35 +0900

    feat: reducer 와 store 의 기능을 추상화해 index 에 정의
    - store 를 생성하고, 상태를 변경시키는 메스드를 정의하고 Export 함

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import { createStore } from '@/store/createStore';
 import { reducer } from '@/store/reducer';
 
-const store = createStore(reducer);
+export const store = createStore(reducer);
+export const actionType = {
+  INIT: 'INIT',
+  DELETE_ITEM: 'DELETE_ITEM',
+};
 
-export default store;
+export const storeInit = (payload) => ({ type: actionType.INIT, payload });
+export const deleteItem = (payload) => ({ type: actionType.DELETE_ITEM, payload });
