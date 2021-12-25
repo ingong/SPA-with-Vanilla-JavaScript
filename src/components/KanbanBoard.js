73294a49baafb3f16aa44e6ab074c2dd3db577fd
ingong <insong1216@gmail.com>
2021-12-25 23:38:26 +0900

    feat: kanban column 에 대해 dropEvent 핸들링
    - 칸반 칼럼의 빈 영역에 드랍되는 경우에 아이템의 order 와 상태가 변하게함
    - 아직 order 를 부여하는 기준은 명확히 하지 않음

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -2,10 +2,13 @@ import Component from '@/common/Component';
 import { qs } from '@/utils/helper';
 import KanbanColumn from '@/components/KanbanColumn';
 import Modal from '@/common/Modal';
-import { store, storeInit } from '@/store/index';
+import { store, storeInit, updateItem } from '@/store/index';
 import '@/style/kanban.css';
 
 export default class KanbanBoard extends Component {
+  // TODO $ 와 # 의 차이를 구분해서 사용
+  $draggingItem;
+
   template() {
     return `
       <main>
@@ -24,6 +27,7 @@ export default class KanbanBoard extends Component {
     ['TODO', 'IN_PROGRESS', 'DONE'].map(
       (value) =>
         new KanbanColumn('.kanban-container', {
+          draggingItem: this.$draggingItem,
           name: value,
           list: store
             .getState()
@@ -31,7 +35,22 @@ export default class KanbanBoard extends Component {
             .sort((a, b) => a.order - b.order),
         }),
     );
+
     new Modal('.kanban-container');
+  }
+
+  // TODO EventListener 제거
+  setEvent() {
+    qs('.kanban-container').addEventListener('dragstart', (e) => {
+      const { id, status } = e.target.dataset;
+      this.$draggingItem = { id, status };
+    });
+
+    qs('.kanban-container').addEventListener('drop', (e) =>
+      handleDropinColumn(e.target.dataset, this.$draggingItem, store.getState()),
+    );
+
+    qs('.kanban-container').addEventListener('dragover', (e) => e.preventDefault());
   }
 
   setUp() {
@@ -40,3 +59,19 @@ export default class KanbanBoard extends Component {
     storeInit();
   }
 }
+
+const handleDropinColumn = ({ id, status }, draggingItem, itemList) => {
+  if (id !== 'column') return;
+  const targetItem = itemList.find((v) => v.id === draggingItem.id);
+  store.dispatch(updateItem({ ...targetItem, status, order: 1 }));
+
+  // 칼럼을 기준으로 나눔
+  // if 놓여진 위치가 새로운 컬럼
+  // else if 놓여진 위치가 동일한 칼럼
+  // 둘 다 order 를 최신화해야함
+
+  // 아이템 기준
+  // 아이템의 최상단 -> 2개의 아이템의 변화가 필요함 (첫 번째꺼 0, 두 번째꺼는 기존 : 0 + 기존 두 번째)
+  // 아이템의 중간 -> drop 된 곳을 기준으로 order 를 계산하고 본인것만 최신화
+  // 아이템의 맨 마지막 -> 기존 존재하는 배열에서
+};
