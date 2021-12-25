39bd3f3b2fc25eba7b80dbbb69744a05c4fa75f8
ingong <insong1216@gmail.com>
2021-12-26 04:03:12 +0900

    refactor: modal 에 작성한 이벤트 핸들러 함수 제거
    - deleteModal 과 defaultModal 에서 각각 메서드 작성 예정

--- src/common/Modal.js
+++ src/common/Modal.js
@@ -1,12 +1,12 @@
 import Component from '@/common/Component';
-import { qs } from '@/utils/helper';
 import '@/style/modal.css';
-import { store, deleteItem } from '@/store/index';
 
 export default class Modal extends Component {
+  $state;
+
   template() {
     return `
-      <div class="modal__container hidden">
+      <div class="modal__container ${this.$state.category} hidden">
         <div class="modal__overlay">
           <div class="modal">
             <div class="modal__content"></div>
@@ -18,25 +18,5 @@ export default class Modal extends Component {
         </div>
       </div>
     `;
-  }
-
-  handleClose(e, exceptList = []) {
-    if (e.target.className === 'modal__content') return;
-    const modalContainerSelector = qs('.modal__container');
-    modalContainerSelector.classList.add('hidden');
-    // 버튼을 클릭하거나, input 을 클릭 하는 경우에 대해서도 예외 처리가 필요함
-  }
-
-  handleConfirmBtn(e, fn, exceptList = []) {
-    // fn();
-    store.dispatch(deleteItem({ id: 'ISSUE-104' }));
-    console.log(this);
-    this.handleClose(e);
-  }
-
-  setEvent() {
-    qs('.modal__overlay').addEventListener('click', (e) => this.handleClose(e));
-    qs('.modal__cancel').addEventListener('click', (e) => this.handleClose(e));
-    qs('.modal__confirm').addEventListener('click', (e) => this.handleConfirmBtn(e));
   }
 }
