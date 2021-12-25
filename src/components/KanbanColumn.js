4367cb84b977f3b83ea3eedb09b99759d923d2ff
ingong <insong1216@gmail.com>
2021-12-26 02:06:48 +0900

    feat: 칸반의 최상단으로 이동시키는 상황에 대한 이동 기능
    - 기존의 아이템의 order 를 변경시킴
    - 새롭게 맨 위에 추가되는 아이템의 order 의 값은 0 으로 초기화

--- src/components/KanbanColumn.js
+++ src/components/KanbanColumn.js
@@ -1,7 +1,8 @@
 import Component from '@/common/Component';
 import KanbanItem from '@/components/KanbanItem';
 import KanbanAddbtn from '@/components/KanbanAddbtn';
-
+import { qs } from '@/utils/helper';
+import { store, updateItem } from '@/store/index';
 export default class KanbanColumn extends Component {
   template() {
     return `
@@ -9,6 +10,7 @@ export default class KanbanColumn extends Component {
       <div class="kanban-column__title ${this.$state.name}-title">
         <h3>${this.$state.name}</h3>
       </div>
+      <div class="kanban__column-line ${this.$state.name}-line" data-status=${this.$state.name}></div>
       <ul class=${this.$state.name}-list></ul>
     </section>
     `;
@@ -29,4 +31,44 @@ export default class KanbanColumn extends Component {
         }),
     );
   }
+
+  setEvent() {
+    const divideLineSelector = qs(`.${this.$state.name}-line`);
+
+    divideLineSelector.addEventListener('dragenter', () => {
+      divideLineSelector.classList.add('drag__enter');
+    });
+
+    divideLineSelector.addEventListener('dragleave', (e) => {
+      divideLineSelector.classList.remove('drag__enter');
+    });
+
+    divideLineSelector.addEventListener('drop', (e) => {
+      divideLineSelector.classList.remove('drag__enter');
+      handleDropItemAtTop(e);
+    });
+  }
 }
+
+const handleDropItemAtTop = ({ currentTarget }) => {
+  const itemList = store.getState();
+  const targetStatus = currentTarget.dataset.status;
+  const filteredList = itemList
+    .filter((item) => item.status === targetStatus)
+    .sort((a, b) => a.order - b.order);
+  const firstItem = filteredList[0] || [];
+
+  const useRefSelector = qs('.useRef');
+  const dragItem = itemList.find((item) => item.id === useRefSelector.dataset.id);
+
+  store.dispatch(updateItem({ ...dragItem, status: targetStatus, order: 0 }));
+
+  if (filteredList.length > 1) {
+    const frontTwoItemList = filteredList.slice(0, 2);
+    const orderSum = frontTwoItemList.map((item) => item.order).reduce((acc, cur) => acc + cur, 0);
+    const orderAvg = orderSum / 2;
+    store.dispatch(updateItem({ ...firstItem, order: orderAvg }));
+  } else if (filteredList.length === 1) {
+    store.dispatch(updateItem({ ...firstItem, order: 1 }));
+  }
+};
