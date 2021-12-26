8625ff568ce92ccd77ce3c7d91a62c74cfd43120
ingong <insong1216@gmail.com>
2021-12-26 21:34:07 +0900

    chore: reducer 에서 default 의 경우 state 반환
    - default 로 state 를 반환하도록 작성

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,14 @@
 export const reducer = (state = [], action = {}) => {
   switch (action.type) {
     case 'INIT_LIST':
-      return action.payload;
+      return [...action.payload];
     case 'DELETE_ITEM':
       return state.filter((value) => value.id !== action.payload.id);
     case 'UPDATE_ITEM':
       return [...state.filter((value) => value.id !== action.payload.id), action.payload];
     case 'CREATE_ITEM':
       return [...state, action.payload];
+    default:
+      return state;
   }
 };
