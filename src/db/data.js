47ec0f4379ebfc3c31eec3863f4d2c0c0f4291e7
ingong <insong1216@gmail.com>
2021-12-26 20:50:03 +0900

    chore: 초기 data 값 수정
    - 기존 3개의 아이템들에서 5개로 추가함
    - 테스트 케이스를 늘리기 위해 추가적인 작성을 함

--- src/db/data.js
+++ src/db/data.js
@@ -1,12 +1,12 @@
 const data = {
-  lastId: 3,
+  lastId: 5,
   itemList: [
     {
       status: 'TODO',
       title: '첫 번째 TO DO ',
       inChargeId: '테스트 유저1',
       id: 'ISSUE-1',
-      order: 1,
+      order: 0,
       lastModifiedTime: '2021-12-26 16:40:10',
     },
     {
@@ -14,7 +14,7 @@ const data = {
       title: '두 번째 TODO',
       inChargeId: '테스트 유저2',
       id: 'ISSUE-2',
-      order: 0,
+      order: 1,
       lastModifiedTime: '2021-12-26 16:45:10',
     },
     {
@@ -22,8 +22,24 @@ const data = {
       title: '세 번째 TODO',
       inChargeId: '테스트 유저3',
       id: 'ISSUE-3',
-      order: 0,
+      order: 5,
       lastModifiedTime: '2021-12-26 16:45:10',
+    },
+    {
+      status: 'IN_PROGRESS',
+      title: '네 번째 TODO',
+      inChargeId: '테스트 유저4',
+      id: 'ISSUE-4',
+      order: 0,
+      lastModifiedTime: '2021-12-26 16:43:10',
+    },
+    {
+      status: 'IN_PROGRESS',
+      title: '다섯 번째 TODO',
+      inChargeId: '테스트 유저5',
+      id: 'ISSUE-5',
+      order: 2,
+      lastModifiedTime: '2021-12-26 16:50:10',
     },
   ],
 };
