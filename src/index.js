f07ac39366bdcef41beb9a0f98a559937110e315
ingong <insong1216@gmail.com>
2022-03-22 01:46:54 +0900

    refactor: index.js 내 App 파일 분리

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-import KanbanBoard from '@/components/KanbanBoard';
-import '@/style/index.css';
-import { storeInit, store } from '@/store';
-import localDB from '@/db';
+import App from '@/App';
 
-const App = {
-  init: async () => {
-    try {
-      const result = await localDB.get();
-      store.dispatch(storeInit(result));
-      new KanbanBoard('#root');
-    } catch (error) {
-      console.log(error);
-    }
-  },
-};
-
-document.addEventListener('DOMContentLoaded', App.init());
+document.addEventListener('DOMContentLoaded', () => App.route());
+window.addEventListener('popstate', () => App.route());
