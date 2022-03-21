564aee00fb968a75346a0c6d3499b142cfc9e717
ingong <insong1216@gmail.com>
2022-03-22 01:03:36 +0900

    feat: webpack css module 관련 설정 및 sourcemap 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const mode = process.env.NODE_ENV;
 
 module.exports = {
   mode: 'development',
-
-  devServer: {
-    host: 'localhost',
-    port: 8080,
-    historyApiFallback: true,
-    hot: true,
+  output: {
+    path: path.resolve(__dirname, 'build'),
   },
-
+  devtool: mode === 'development' ? 'eval-source-map' : false,
   module: {
     rules: [
       {
@@ -20,22 +17,29 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        resolve: {
-          alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@public': path.resolve(__dirname, 'public'),
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: mode === 'development' ? '[path]__[local]' : '[hash:base64]',
+              },
+            },
           },
-        },
+        ],
       },
     ],
   },
-
   plugins: [
     new HtmlWebpackPlugin({
       template: '/public/index.html',
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@public': path.resolve(__dirname, 'public'),
+    },
+  },
 };
