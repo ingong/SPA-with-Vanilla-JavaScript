8650680b9548a2e3647b1aeaa1008fa4cc04e682
ingong <insong1216@gmail.com>
2022-03-30 12:14:28 +0900

    [CONFIG]: typescript 설치 및 babel 설정 작성

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|ts)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
