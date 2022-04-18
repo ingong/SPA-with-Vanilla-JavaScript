8bdd7a59faaa2d906bc3673a5f4e71013fc864ab
ingong <insong1216@gmail.com>
2022-04-18 11:25:04 +0900

    [FEAT]: LanguageSearch 컴포넌트 추가

---
+++ src/components/LanguageSearch/LanguageSearchPage.ts
@@ -0,0 +1,54 @@
+import { qs } from '@/utils/helper';
+import '@/components/LanguageSearch/style/page.scss';
+
+interface ObjectType {
+  count: number;
+}
+
+export default class CounterPage {
+  $target;
+  #state;
+
+  constructor($target: string) {
+    this.$target = $target;
+    this.#state = { count: 0 };
+    this.render();
+    this.setEvent();
+  }
+
+  setState(newState: ObjectType) {
+    this.cleanup();
+    this.#state = { ...this.#state, ...newState };
+    this.render();
+    this.setEvent();
+  }
+
+  template() {
+    return `<button class="plus">plus</button><span>${this.#state.count}</span><button class="minus">minus</button>`;
+  }
+
+  render() {
+    qs(this.$target).insertAdjacentHTML('beforeend', this.template());
+    this.mountChild();
+  }
+
+  mountChild() {}
+
+  setEvent() {
+    this.addEvent('.plus', 'click', () => {
+      this.setState({ count: this.#state.count + 1 });
+    });
+    this.addEvent('.minus', 'click', () => {
+      this.setState({ count: this.#state.count - 1 });
+    });
+  }
+
+  addEvent(target: string, eventType: string, callback: () => void) {
+    if (!qs(target)) throw 'no element found';
+    qs(target).addEventListener(eventType, callback);
+  }
+
+  cleanup() {
+    qs('#root').replaceChildren();
+  }
+}
